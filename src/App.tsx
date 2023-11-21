0b992440e41390a29feb8cdbe1387997f13e553f
lazlanrafar <lazlanrafar@gmail.com>
2023-11-21 11:10:16 +0700

    setup(routes): install and setup react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/sign-in" element={<h1>Sign In</h1>} />
+
+      <Route index path="/" element={<h1>Home</h1>} />
+    </Routes>
+  );
 };
