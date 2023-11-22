fb9bb41eded33b9103cc1c74a60c885930233387
lazlanrafar <lazlanrafar@gmail.com>
2023-11-22 11:04:04 +0700

    feat(shadcn): install and setup shadcn/ui

--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function LayoutAuth() {
   return (
-    <div>
+    <div className="w-full min-h-screen flex items-center justify-center">
       <Outlet />
     </div>
   );
--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -1,3 +1,10 @@
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  return (
+    <div className="max-w-md w-full p-5 border rounded-lg">
+      <h1 className="text-xl font-medium mb-2">Instagram Clone</h1>
+      <p className="text-gray-500 font-light text-sm">
+        This is a clone of Instagram using React, Tailwind CSS, and Firebase. This is the sign in page.
+      </p>
+    </div>
+  );
 }
